eb0636f2fdc0f2076812d056fa9df47194579017
ESarcomere <169079855+ESarcomere@users.noreply.github.com>
2024-11-28 01:21:28 -0800

    Fixing velocity PID

--- src/main.cpp
+++ src/main.cpp
@@ -15,23 +15,33 @@
 HFIBLDCMotor motor = HFIBLDCMotor(1,19.7);
 // PIDController P_angle{motor.P_angle.P,motor.P_angle.I,motor.P_angle.D,motor.P_angle.output_ramp,300};	//!< parameter determining the position PID configuration 
 // PIDController PID_angle = PIDController(motor.P_angle.P,motor.P_angle.I,motor.P_angle.D,0,250);
+// PIDController PID_velocity{DEF_PID_VEL_P,DEF_PID_VEL_I,DEF_PID_VEL_D,DEF_PID_VEL_RAMP,DEF_PID_VEL_LIMIT};//!< parameter determining the velocity PID configuration
+
+
 // LowPassFilter LPF_angle = LowPassFilter(1/(2000*_2PI));
 // BLDCMotor motor = BLDCMotor(1,19.6);
 BLDCDriver6PWM driver = BLDCDriver6PWM(PA10, PB1, PA9 ,PB0 ,PA8 , PA7);
 
 
-// LowsideCurrentSense current_sense = LowsideCurrentSense(0.2f, 20.0f, PA4, PA5, PA6);
+LowsideCurrentSense current_sense = LowsideCurrentSense(0.2f, 0.15f, PA4, PA5, PA6);
 // MagneticSensorAS5048A sensor(PB6);
 
-// MagneticSensorSPI sensor = MagneticSensorSPI(AS5048_SPI, PB6);
-// HallSensor h_sensor = HallSensor(PA0, PA1, PA2, 1);
+MagneticSensorSPI m_sensor = MagneticSensorSPI(MA730_SPI, PA13);
+SPIClass SPI_2(PB15, PB14, PB13);
 
+
+
+HallSensor h_sensor = HallSensor(PA0, PA1, PA2, 1);
+// interrupt routine initialization
+void doA(){h_sensor.handleA();}
+void doB(){h_sensor.handleB();}
+void doC(){h_sensor.handleC();}
 
 
 
 
 // instantiate the commander
-float target_voltage = 10;
+float target_voltage = 0;
 float lpf_ang;
 
 Commander command = Commander(Serial);
@@ -60,14 +70,15 @@ int motion_flag= 1;
 float state_flag= 1;
 float delta;
 float h_sensor_velocity = 0;
+float vq;
 
 
-// void doAngle(char* cmd) { command.scalar(&target_angle, cmd); }
+void doAngle(char* cmd) { command.scalar(&target_angle, cmd); }
 // void lpfD(char* cmd) { command.scalar(&motor.LPF_current_d.Tf, cmd); }
 // void lpfQ(char* cmd) { command.scalar(&motor.LPF_current_q.Tf, cmd); }
-// void pP(char* cmd) { command.scalar(&PID_angle.P, cmd); }
-// void pI(char* cmd) { command.scalar(&PID_angle.I, cmd); }
-// void pD(char* cmd) { command.scalar(&PID_angle.D, cmd); }
+void pP(char* cmd) { command.scalar(&motor.PID_velocity.P, cmd); }
+void pI(char* cmd) { command.scalar(&motor.PID_velocity.I, cmd); }
+void pD(char* cmd) { command.scalar(&motor.PID_velocity.D, cmd); }
 // void sF(char* cmd) { command.scalar(&state_flag, cmd); }
 // void LPF(char* cmd) { command.scalar(&LPF_angle.Tf, cmd); }
 // void OR(char* cmd) { command.scalar(&PID_angle.output_ramp, cmd); }
@@ -112,23 +123,29 @@ void setup() {
 
   // power supply voltage [V]
   motor.useMonitoring(Serial);
-  driver.voltage_power_supply = 12;
-  motor.voltage_sensor_align = 12;
+  driver.voltage_power_supply = 24;
+  motor.voltage_sensor_align = 5; 
   
   // limit the maximal dc voltage the driver can set
   // as a protection measure for the low-resistance motors
   // this value is fixed on startup
-  driver.voltage_limit = 12;
+  driver.voltage_limit = 24;
   driver.pwm_frequency = 20000;
   // driver.enable_active_high = false;
   driver.dead_zone = 0.004;
 
+
   
 
   driver.init();
+  h_sensor.init();
+  m_sensor.init(&SPI_2);
+
+  // enable hall sensor hardware interrupts
+  h_sensor.enableInterrupts(doA, doB, doC); 
   // current_sense.linkDriver(&driver);
   // motor.linkCurrentSense(&current_sense);
-  // motor.linkSensor(&h_sensor);
+  motor.linkSensor(&h_sensor);
   // motor.linkSensor(&sensor);
 
  
@@ -141,9 +158,8 @@ void setup() {
 
 
 
-  // driver.enable();
+  driver.enable();
   // current_sense.init();
-  // h_sensor.init();
 
 
 
@@ -152,7 +168,7 @@ void setup() {
   // limit the voltage to be set to the motor
   // start very low for high resistance motors
   // current = voltage / resistance, so try to be well under 1Amp
-  motor.current_limit= 0.25f;   
+  // motor.current_limit= 0.25f;   
   // PID_angle.P = 750.0f;
   // PID_angle.I = 6.0f;
   // PID_angle.D = 0.01f;
@@ -172,14 +188,18 @@ void setup() {
  
   // open loop control config
   motor.torque_controller = TorqueControlType::voltage;
-  motor.controller = MotionControlType::velocity_openloop;
+  motor.controller = MotionControlType::velocity;
   motor.foc_modulation = FOCModulationType::SpaceVectorPWM;
+  motor.PID_velocity.P = 0.2;
+  motor.PID_velocity.I = 0.0;
+  motor.PID_velocity.D = 0.001;
+  motor.PID_velocity.output_ramp = 1000;
+  
 
-  motor.Ld = 172.95e-6f;
-  motor.Lq = 232.75e-6f;
+  // motor.Ld = 172.95e-6f;
+  // motor.Lq = 232.75e-6f;
   // motor.hfi_v = 6;
   // motor.hfi_on = true;
-  motor.voltage_sensor_align = 5;
 
   
   // motor.sensor_direction = CCW;
@@ -201,7 +221,7 @@ void setup() {
 
   // add target command T
   command.add('T', doTarget, "target voltage");
-  // command.add('G', doAngle, "target angle");
+  command.add('G', doAngle, "target angle");
   // command.add('P', pP, " proportional");
   // command.add('I', pI, " intergral");
   // command.add('D', pD, " differential");
@@ -245,13 +265,32 @@ void setup() {
 
 
 uint32_t time_prev=0;
+uint32_t time_p=0;
+
 void loop() {
+
+   
+    // Read the incoming data into a buffer
+    
+
+    // Optionally process the received message (e.g., as a command)
+    // Example: Setting motor target velocity
+    // if (strncmp(receivedData, "velocity:", 9) == 0) {
+    //     float velocity = atof(receivedData + 9); // Extract velocity value
+    //     motor.target = velocity;                // Set motor target
+    //     Serial.print("Set target velocity to: ");
+    //     Serial.println(velocity);
+    // }
+    
 
   
 
 
   motor.loopFOC();
+  // h_sensor.update();
+  m_sensor.update();
 
+  
 
 
 
@@ -261,16 +300,74 @@ void loop() {
   // using motor.voltage_limit and motor.velocity_limit
   // to turn the motor "backwards", just set a negative target_velocity
   uint32_t time_now = micros();
-  if (fabs(time_now-time_prev) > 10000){
+  if (fabs(time_now-time_prev) > 1000){
     time_prev = time_now;
-    motor.move(2);
-    Serial.println(digitalRead(PC13));
+    if(target_voltage == 0.0f ){
+      motor.PID_velocity.reset();
+    }
+    motor.move(target_voltage - h_sensor.vel);
+    
 
-    // Serial.print(driver.dc_a);
-    // Serial.println("  ");
-    // Serial.print(driver.dc_b);
-    // Serial.println("  ");
-    // Serial.println(driver.dc_a);
+
+    
+
+    
+
+    // Serial.println(digitalRead(PC13));
+    // Serial.print(motor.enabled);
+  }
+  uint32_t time_n = micros();
+  if (fabs(time_n-time_p) > 1000000){
+    time_p = time_n;
+    Serial.print("Angle:");
+    Serial.println(h_sensor.getSensorAngle());
+    Serial.print("Velocity:");
+    Serial.println(h_sensor.getVelocity());
+    // Serial.print("Shaft:");
+    // Serial.println(motor.shaft_velocity);
+    Serial.println("-------");
+
+    Serial.print("Target:");
+    Serial.println(target_voltage);
+    Serial.print("V..Q:");
+    Serial.println(motor.voltage.q);
+    Serial.print("V..D:");
+    Serial.println(motor.voltage.d);
+    Serial.print("U.A:");
+    Serial.println(motor.Ua);
+    Serial.print("HFI:");
+    Serial.println(motor.hfi_velocity);
+    Serial.print("Temp:");
+    Serial.println(motor.current_setpoint.q);
+    // Serial.print("Hall:");
+    // Serial.println(h_sensor.getVelocity());
+    Serial.print("Shaft:");
+    Serial.println(motor.shaft_velocity);
+    Serial.println("-------");
+
+    
+
+
+  //   // char receivedData[128]; // Adjust size based on expected input
+  //   // size_t dataIndex = 0;
+
+  //   // while (Serial.available() && dataIndex < sizeof(receivedData) - 1) {
+  //   //     receivedData[dataIndex++] = Serial.read();
+  //   // }
+  //   // receivedData[dataIndex] = '\0'; // Null-terminate the string
+
+  //   // // Echo the received data back to the sender
+  //   // Serial.print("Received: ");
+  //   // Serial.println(receivedData);
+
+
+
+
+
+
+  //   // ABCurrent_s s = current_sense.getABCurrents(current_sense.getPhaseCurrents());
+  //   // Serial.print(s.alpha);
+  //   // Serial.print(s.beta);
   }
 
   // // Motion control function
@@ -279,7 +376,8 @@ void loop() {
   // // You can also use motor.move() and set the motor.target in the code
   
   // // user communication
-  // command.run();
+  command.run();
+  
   
 
   // // // has to be called before getAngle and getVelocity
